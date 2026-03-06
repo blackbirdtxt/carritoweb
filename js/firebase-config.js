a0dc5dac39fc2eb3216c41a39f11e8c8c17289c7
blackbirdtxt <oscarsitoequisde@gmail.com>
2026-03-06 15:31:18 -0500

    Update Firebase configuration with real credentials

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,14 +4,14 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "REPLACE",
-  authDomain: "REPLACE",
-  projectId: "REPLACE",
-  storageBucket: "REPLACE",
-  messagingSenderId: "REPLACE",
-  appId: "REPLACE"
+  apiKey: "AIzaSyCochv627I1dkM_brNkYMmJyH9FO5LJgNI",
+  authDomain: "carrito-web-928bf.firebaseapp.com",
+  projectId: "carrito-web-928bf",
+  storageBucket: "carrito-web-928bf.firebasestorage.app",
+  messagingSenderId: "410039719184",
+  appId: "1:410039719184:web:e2aa06ce2216c862f78195",
+  measurementId: "G-C741QWJ9D7"
 };
-
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
